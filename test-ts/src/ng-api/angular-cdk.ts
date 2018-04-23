1ea421412c342734b3074fd790e6391051f6290a
gntower2013t <chensg2008@gmail.com>
2018-04-23 18:27:29 +0800

    from home

--- test-ts/src/ng-api/angular-cdk.ts
+++ test-ts/src/ng-api/angular-cdk.ts
@@ -1,4 +1,9 @@
+import { Component, OnInit, ComponentFactoryResolver, Injector, ApplicationRef } from "@angular/core";
 
+/*
+	render this component in a DOM element running outside the Angular context
+	ApplicationRef
+*/
 @Component({
 	selector: 'app-root',
 	template: ``
@@ -8,7 +13,8 @@ export class AppComponent implements OnInit {
 
 	constructor(
 		private componentFactoryResolver: ComponentFactoryResolver,
-		private injector: Injector
+		private injector: Injector,
+		private applicationRef: ApplicationRef
 	) { }
 
 	ngOnInit() {
@@ -16,9 +22,17 @@ export class AppComponent implements OnInit {
 		const headerElement = document.querySelector('#pageHeader');
 		// Locate the component factory for the HeaderComponent
 		const componentFactory = this.componentFactoryResolver.resolveComponentFactory(HeaderComponent);
-		// Generate an instance of the HeaderComponent
+		// Generate an instance of the HeaderComponent, on headerElement
 		this.componentRef = componentFactory.create(this.injector, [], headerElement);
 		// Attach to the component to Angular's component tree for dirty checking
 		this.applicationRef.attachView(this.componentRef.hostView);
+	}
+
+	/* manual trigger change detection, angular cdk solve this problem
+	  portal hosts
+		https://blog.angularindepth.com/angular-cdk-portals-b02f66dd020c
+	*/
+	doCheck() {
+		this.componentRef.changeDetectorRef.detectChanges();
 	}
 }
--- test-ts/src/ng-api/boot-strap.ts
+++ test-ts/src/ng-api/boot-strap.ts
@@ -1,4 +1,22 @@
-import { AppModule, AppModule2 } from './app';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
+/* bootstrap two apps
+	two man.ts ?? no need ??
+
+	Note from official code:
+	https://github.com/angular/angular/blob/ae75e3640a2d9eb1e897a0771d92b976c5a42c75/modules/%40angular/platform-browser-dynamic/index.ts#L87
+ * ## Bootstrapping Multiple Applications
+ *
+ * When working within a browser window, there are many singleton resources: cookies, title,
+ * location, and others. Angular services that represent these resources must likewise be
+ * shared across all Angular applications that occupy the same browser window. For this
+ * reason, Angular creates exactly one global platform object which stores all shared
+ * services, and each angular application injector has the platform injector as its parent.
+ *
+ * Each application has its own private injector as well. When there are multiple
+ * applications on a page, Angular treats each application injector's services as private
+ * to that application.
+
+*/
 platformBrowserDynamic().bootstrapModule(AppModule)
 platformBrowserDynamic().bootstrapModule(AppModule2)
