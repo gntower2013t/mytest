6b0a9ef874133a5ab199871f75087aae910e97ab
axdum <chensg2008@gmail.com>
2018-05-19 21:30:55 +0800

    office update

---
+++ test-ts/src/e2e-jasmine/e2e.ts
@@ -0,0 +1,9 @@
+import { browser, by, element, $, $$ } from 'protractor';
+
+
+browser.sleep(1000);
+browser.driver.sleep(1000);
+
+elements
+// .each(ele => ele.getText().then(v => console.log("ele: " + v)))
+// .all(by.linkText(name)).first();
